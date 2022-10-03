33e01f25a0e5cc9dfdc951f788b0427f78b7ec00
Benjamin Pang <benjinpang@gmail.com>
2022-10-03 14:04:56 -0400

    title -> work

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-new LazyLoad({})
+let ll = new LazyLoad({})
+
+$(_ => {
+  $('.tabs li').on('click', e => {
+    let keep = $(e.target).parent('li')
+    if (!keep.hasClass('is-active')) {
+      let ktxt = keep.text()
+      let show = $('section')
+
+      $('.tabs li').removeClass('is-active')
+      keep.addClass('is-active')
+
+      show.addClass('oh')
+      setTimeout(_ => {
+        show.addClass('dh')
+        if (ktxt != 'all') show = show.filter($('.' + ktxt))
+        setTimeout(_ => { show.removeClass('dh oh') }, 50)
+      }, 200)
+    }
+  })
+})
